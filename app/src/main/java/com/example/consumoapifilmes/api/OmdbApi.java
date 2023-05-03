505b084b408d6fdd8127e8a959e5d8ef6214f7f0
gui-sl <guilhermesluiz03@gmail.com>
2023-05-03 11:56:05 -0300

    first commit

---
+++ app/src/main/java/com/example/consumoapifilmes/api/OmdbApi.java
@@ -0,0 +1,16 @@
+package com.example.consumoapifilmes.api;
+
+import com.example.consumoapifilmes.model.FilmeModel;
+import com.example.consumoapifilmes.model.FilmeSerializable;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface OmdbApi{
+
+    @GET("?apikey=4369d39a&t=matrix")
+    Call<FilmeModel> getFilme();
+
+    @GET("?apikey=4369d39a&s=matrix")
+    Call<FilmeSerializable> getFilmes();
+}
